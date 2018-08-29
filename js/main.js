441fc57751ca12f1654f1fedf9ae3d07f260c476
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-08-29 19:17:42 +0530

    added click visual effects

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 (function() {
   // Variable Declarations
   let handburgers = document.querySelectorAll(".handburger");
+  let links = document.querySelectorAll("aside ul.sidenav li");
+  let activeClass = 0;
 
   // Toggle Menu items
   function handburgerClicked() {
     this.parentNode.classList.toggle("active");
   }
 
-  // Adding click event listner for all the handburger icons
+  function changeActive(index) {
+    links[activeClass].classList.remove("active");
+    activeClass = index;
+    links[activeClass].classList.add("active");
+  }
+
+  // Adding click event listener for all the handburger icons
   handburgers.forEach(handburger => {
     handburger.addEventListener("click", handburgerClicked);
+  });
+
+  // Adding click event listener for all the links
+  links.forEach((link, index) => {
+    link.addEventListener("click", changeActive.bind(this, index));
   });
 })();
