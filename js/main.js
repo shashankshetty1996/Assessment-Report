68990997927954a82630d00a3cdd29ae0b8be535
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-08-29 20:03:37 +0530

    Added model box

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-(function() {
+let nav = (function() {
   // Variable Declarations
-  let handburgers = document.querySelectorAll(".handburger");
-  let links = document.querySelectorAll("aside ul.sidenav li");
-  let activeClass = 0;
+  let handburgers = document.querySelectorAll(".handburger"),
+    links = document.querySelectorAll("aside ul.sidenav li"),
+    activeClass = 0;
 
   // Toggle Menu items
   function handburgerClicked() {
@@ -23,5 +23,35 @@
   // Adding click event listener for all the links
   links.forEach((link, index) => {
     link.addEventListener("click", changeActive.bind(this, index));
+  });
+})();
+
+let model = (function() {
+  // Variable Declarations
+  let boxes = document.querySelectorAll(".box"),
+    modelCloseBtn = document.querySelectorAll("a[data-close]");
+
+  // Display model functions
+  function modelShow() {
+    // Model type to be displayed
+    let targetId = this.getAttribute("data-target");
+    document.getElementById(targetId).classList.add("active");
+  }
+
+  // Close display model
+  function modelClose(e) {
+    e.preventDefault();
+    let targetId = this.getAttribute("data-close");
+    document.getElementById(targetId).classList.remove("active");
+  }
+
+  // Adding click event for the box
+  boxes.forEach(box => {
+    box.addEventListener("click", modelShow);
+  });
+
+  // Close model box
+  modelCloseBtn.forEach(btn => {
+    btn.addEventListener("click", modelClose);
   });
 })();
